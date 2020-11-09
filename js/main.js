b8fb89ae61befe7a5566ce3e622964fbee7cc210
OlivierFrancois <olivier.francois09@gmail.com>
2020-11-09 16:40:00 +0100

    switch de smalldata à largedata

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload = async function () {
-    
+    //------------ Démarage de la page ------------//
     'use strict';
 
     //Le code du projet sera inséré ici
@@ -13,32 +13,49 @@ window.onload = async function () {
     let thead = container.append('thead').classed('thead-dark', true).append('tr');
 
     // On peut enter car on ne fait qu'ajouter des choses
-    thead.selectAll('th').data(data.columns).enter().append('th').text((d) => d);
+    thead.selectAll('th').data(data.columns)
+    .enter().append('th')
+    .text((d) => d);
 
     // Corps du tableau
     let tbody = container.append('tbody');
-    let tr = tbody.selectAll('tr').data(data).enter().append('tr');
+    // .data(data) permet de créer autant de tr qu'il y a de ligne dans le tableau data
+    let lines = tbody.selectAll('tr').data(data)
+    .enter().append('tr');
     
-    let td = tr
-    .selectAll('td')
+
+    let cells = lines.selectAll('td')
+    // Prend la data contenue dans le tr et en extrait les valeurs
+    // pour faire autant de td qu'il y a de couples clés/valeurs
     .data( (dataTr) => Object.values(dataTr) )
-    .enter()
-    .append('td')
+    .enter().append('td')
     .text(d => d);
 
-    //tr.selectAll('td').text( (data) => data.Name).append('td');
 
-    document.querySelector("#btn-larger-data").addEventListener('click', async () => {
-        data = await d3.tsv('./data/large-data.tsv')
+    //------------ Boutons ------------//
+
+    let dataFlag = 1;
+    const switchData = document.querySelector("#switch-data")
+    switchData.addEventListener('click', async () => {
+        if (dataFlag == 1) {
+            switchData.innerText = "Charger Large Data";
+            dataFlag = 2;
+            source = './data/small-data.tsv';
+        } else {
+            dataFlag = 1;
+            source = './data/large-data.tsv';
+            switchData.innerText = "Charger Small Data";
+        }
+
+        data = await d3.tsv(source);
+
+        let tlines = tbody.selectAll('tr').data(data);
         
-        let tbody = container.append('tbody');
-        let tr = tbody.selectAll('tr').data(data).enter().append('tr');
+        tlines.exit().remove();
+        tlines = tlines.enter().append('tr').merge(tlines);
         
-        let td = tr
-        .selectAll('td')
-        .data( (dataTr) => Object.values(dataTr) )
-        .enter()
-        .append('td')
-        .text(d => d);
+        let tcells = tlines.selectAll('td').data((d) => Object.values(d))
+        .enter().append('td')
+        .text((d) => d);
     })
 }
